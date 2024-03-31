72065d6b63f5c238f1f39e20554b5c4daacc4e34
GirlScoutMintThins <165102712+GirlScoutMintThins@users.noreply.github.com>
2024-03-31 19:16:45 -0400

    Create Scouts.lua

---
+++ Scouts.lua
@@ -0,0 +1,60 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GirlScoutMintThins/Adjusting/main/MintAimbot.lua"))()
+
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+-- Settings
+
+bind = "" 
+bind2 = "h"
+bind3 = "y"
+
+-- Script
+
+mouse.KeyDown:connect(function(key)
+if key == bind then
+player.Character.HumanoidRootPart.CFrame = CFrame.new(1254.09656, 137.906067, -172.128204)
+end
+end)
+
+mouse.KeyDown:connect(function(key2)
+if key2 == bind2 then
+getgenv().HeadSize = 0.001
+getgenv().Disabled = true
+
+
+if getgenv().Disabled then
+for i,v in next, game:GetService('Players'):GetPlayers() do
+if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+pcall(function()
+v.Character.HumanoidRootPart.Size = Vector3.new(getgenv().HeadSize,getgenv().HeadSize,getgenv().HeadSize)
+v.Character.HumanoidRootPart.Transparency = 1
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
+v.Character.HumanoidRootPart.Material = "Neon"
+v.Character.HumanoidRootPart.CanCollide = true
+end)
+end
+end
+end
+end
+end)
+
+mouse.KeyDown:connect(function(key3)
+if key3 == bind3 then
+getgenv().HeadSize = 1
+getgenv().Disabled = true
+
+
+for i,v in next, game:GetService('Players'):GetPlayers() do
+if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+pcall(function()
+v.Character.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+v.Character.HumanoidRootPart.Transparency = 1
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Medium stone grey")
+v.Character.HumanoidRootPart.Material = "Plastic"
+v.Character.HumanoidRootPart.CanCollide = true
+end)
+end
+end
+end
+end)
